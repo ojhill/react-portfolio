29549ab8f2d491f428f4f654acb402f65b784418
Orenthal Hill <ojhill98@gmail.com>
2021-12-27 17:18:45 -0600

    personal page work

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  printWidth: 120,
+};
---
+++ Layout.js
@@ -0,0 +1,16 @@
+import styles from 'styles/layout.module.css';
+import Link from 'next/link';
+export default function Layout({ children }) {
+  return (
+    <div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  );
+}
